76e5123fbf70bfa07c8364926b9d3e1db1e8fff1
Berkut4713 <av.vecherov@gmail.com>
2022-12-22 13:30:16 +0400

    feat: 15 string

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -256,8 +256,14 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  // if (value === undefined || value === null) {
+  //   return false;
+  // }
+  // if (typeof value === 'object') {
+  //   return true;
+  // }
+  return typeof value === 'string' || value instanceof String;
 }
 
 
