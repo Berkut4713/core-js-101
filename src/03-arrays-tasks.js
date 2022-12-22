e52c28ddf1ea2317cccfd88d7bc21fbd142831a1
Berkut4713 <av.vecherov@gmail.com>
2022-12-22 21:44:03 +0400

    feat: 32 number

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -38,12 +38,6 @@ function findElement(arr, value) {
 function generateOdds(len) {
   const arr = new Array(len);
   const result = arr.fill(0).map((elem, index) => 2 * index + 1);
-  // const arr = [];
-  // for (let i = 1; i <= len;) {
-  //   arr.push(odd);
-  //   odd += 2;
-  //   i += 1;
-  // }
   return result;
 }
 
@@ -60,8 +54,8 @@ function generateOdds(len) {
  *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
  *    [] => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  return [...arr, ...arr];
 }
 
 
@@ -76,10 +70,10 @@ function doubleArray(/* arr */) {
  *    [-1, 2, -5, -4, 0] => [ 2 ]
  *    [] => []
  */
-function getArrayOfPositives(/* arr */) {
-  throw new Error('Not implemented');
+function getArrayOfPositives(arr) {
+  const posArr = arr.filter((num) => num > 0);
+  return posArr;
 }
-
 /**
  * Returns the array with strings only in the specified array (in original order)
  *
