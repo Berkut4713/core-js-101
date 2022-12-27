3197da6f72e5667ba49b312294729a3e14c8c520
Berkut4713 <av.vecherov@gmail.com>
2022-12-27 14:15:41 +0400

    feat: 79 Loops_and_iteration

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -325,8 +325,16 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const numString = String(num);
+  if (numString.length === 1) {
+    return +numString;
+  }
+  let sum = 0;
+  for (let i = 0; i < numString.length; i += 1) {
+    sum += +numString[i];
+  }
+  return getDigitalRoot(sum);git 
 }
 
 
